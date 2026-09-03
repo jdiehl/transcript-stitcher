d816f8954db8aba50790589f5ea553b0d4b7826a
Jonathan Diehl <1334574+jdiehl@users.noreply.github.com>
2026-09-03 10:09:06 +0200

    Initial Commit

---
+++ Transcript Stitcher/Transcript_StitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Transcript_StitcherApp.swift
+//  Transcript Stitcher
+//
+//  Created by Jonathan Diehl on 03/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct Transcript_StitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
